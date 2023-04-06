925903fa09909828825a16785c4395e14c6278d2
ilhamr <hfzrzky489@gmail.com>
2023-04-06 20:54:54 +0700

    menambahkan file

---
+++ lib/model/model_gerakan.dart
@@ -0,0 +1,17 @@
+class ModelGerakan {
+  int? id;
+  String? name;
+  String? arabic;
+  String? latin;
+  String? terjemahan;
+
+  ModelGerakan(this.id, this.name, this.arabic, this.latin, this.terjemahan);
+
+  ModelGerakan.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    arabic = json['arabic'];
+    latin = json['latin'];
+    terjemahan = json['terjemahan'];
+  }
+}
